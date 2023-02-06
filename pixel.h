26a58c608cd0bda782afa2447c08513b45ee7f45
ErwannE <124594762+ErwannE@users.noreply.github.com>
2023-02-06 16:25:32 +0100

    Update pixel.h
    Added left product by scalar and =

--- pixel.h
+++ pixel.h
@@ -12,11 +12,14 @@ public:
     pixel operator-(pixel b); // Difference
     pixel operator*(double lambda); // Multiplication pixel*scalar
     pixel operator/(double lambda); // Multiplication pixel*scalar
+    const pixel& operator=(const pixel& S);
 
     double operator*(pixel b); // Scalar product
     double norm(); // Norm
 
     pixel rotate(double angle); // Rotation (angle in degrees)
 };
+
+pixel operator*(double lambda, pixel p); //Define scalar*pixel
 
 
