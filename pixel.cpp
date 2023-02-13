0c01249213d0d63740a92b8b1eb54bf83fa44e63
Philomène <philomene.boisnard@eleves.enpc.fr>
2023-02-13 14:23:27 +0100

    setPixel

--- pixel.cpp
+++ pixel.cpp
@@ -24,14 +24,18 @@ double pixel::getData(){
     return d;
 }
 double pixel::getPriority(){
-    return c*d
+    return c*d;
 }
 bool pixel::getV(){
     return v;
 }
 
 
-void pixel::set(double x1, double y1){
+void pixel::setPixel(double x1, double y1, Color col1, double c1, double d1, bool v1){
     x=x1;
     y=y1;
+    col=col1;
+    c=c1;
+    d=d1;
+    v=v1;
 }
