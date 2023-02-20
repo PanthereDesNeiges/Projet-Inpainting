4294d6095c02757d20280a3c167d3c109d012512
sinodine <sixtine.nodet@gmail.com>
2023-02-20 15:07:20 +0100

    Pour que matching existe

--- matching.cpp
+++ matching.cpp
@@ -7,33 +7,3 @@ using namespace Imagine;
 
 
 /* ******************************* */
-
-
-// Implémentation du constructeur
-image::image(int width, int height) : w(width), h(height) {
-    // Alloue de la mémoire pour le tableau de pixels
-    data = new pixel[w*h];
-}
-
-// Implémentation du destructeur
-image::~image() {
-    // Libère la mémoire allouée pour le tableau de pixels
-    delete [] data;
-}
-
-// Implémentation des autres méthodes
-int image::getW() {
-    return w;
-}
-
-int image::getH() {
-    return h;
-}
-
-pixel image::getData(int x, int y) {
-    return data[y*w + x];
-}
-
-void image::setPixel(int x, int y, pixel p) {
-    data[y*w + x] = p;
-}
--- matching.h
+++ matching.h
@@ -2,17 +2,3 @@
 #include <Imagine/Graphics.h>
 using namespace Imagine;
 #include "pixel.h"
-
-class image {
-    int w,h;
-    pixel *data; // pointeur vers le tableau de pixels
-
-public:
-    image(int width, int height); // Constructeur
-    ~image(); // Destructeur
-    int getW();
-    int getH();
-    pixel getData(int x, int y);
-    void setPixel(int x, int y, pixel p);
-};
-
