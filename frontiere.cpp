2799043208b83e23491718965f40014298a73cd7
Philomène <philomene.boisnard@eleves.enpc.fr>
2023-02-20 15:06:40 +0100

    frontiere.cpp

--- frontiere.cpp
+++ frontiere.cpp
@@ -1,7 +1,21 @@
-#include "pixel.h"
-#include "image.h"
 #include "frontiere.h"
 #include <cmath>
 using namespace std;
 #include <Imagine/Graphics.h>
 using namespace Imagine;
+
+void add_frontiere(){
+
+}
+
+void initialize_frontiere(){
+
+}
+
+void pop_frontiere(){
+
+}
+
+pixel max_priority(){
+
+}
