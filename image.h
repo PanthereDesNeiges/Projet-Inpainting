1c481c2ae99b5f34249cf38ee50a4f1edec05ed7
sinodine <sixtine.nodet@gmail.com>
2023-02-20 14:30:14 +0100

    Création de la classe image
    Ajout des fichier image.cpp image.h et ajout de quelques fonctions de base associées

---
+++ image.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+
+class pixel {
+    //int x,y;     // position
+    Color col;      // Color
+    double c,d;     // Confidence and Data term
+    bool v;         // 0 if not visited yet, 1 else
+
+public:
+    //int getX();
+    //int getY();
+    Color getColor();
+    double getConfidence();
+    double getData();
+    double getPriority();
+    bool getV();
+
+    void setPixel(Color col1, double c1, double d1, bool v1);
+    void setBlanck();    // To initialize the pixels in the target zone
+};
+
+
