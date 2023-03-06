53e1372721792c75780f888917010be3340513b8
flamschou <flams3630@gmail.com>
2023-03-06 14:56:28 +0100

    header pop frontiere

--- frontiere.h
+++ frontiere.h
@@ -9,6 +9,6 @@ class frontiere {
 public:
     void add_frontiere(std::vector<pixel> v);   // prend en argument la liste de points à partir de laquelle on fait la frontière
     void initialize_frontiere();
-    void pop_frontiere();
+    void pop_frontiere(std::vector<pixel> v);   //prend en argument le petit carré
     pixel max_priority();
 };
