b8a7189d42aecaa77328a0d4224210d6623d1930
Philomène <philomene.boisnard@eleves.enpc.fr>
2023-03-16 11:18:53 +0100

    fin de cours

--- main.cpp
+++ main.cpp
@@ -3,19 +3,37 @@
 // Student(s): Philomène Boisnard, Erwann Estève, Wandrille Flamant, Sixtine  Nodet
 
 #include <Imagine/Graphics.h>
+#include <Imagine/Images.h>
 using namespace Imagine;
 #include "pixel.h"
 
 
 const int width = 512, height = 512;
 
-void f(){
-    openWindow(width,height);
+int f(){
 
-    std::cout<<"Cliquez pour sélectionner les points de la frontière, clic droit pour finir";
+    // CHARGER ET AFFICHER L'IMAGE
+
+    Image<byte> I;
+    const char* fic1 = srcPath("zebra.png");
+    if(! load(I, fic1)) {
+        std::cout << "Probleme dans le chargement d'images" << std::endl;
+        return 1;
+    }
+    openWindow(I.width(),I.height());
+    display(I);
+
+    // SELECTIONNER LES POINTS DU CONTOUR DANS UN VECTEUR
+
+    std::cout<<"Cliquez pour selectionner les points de la frontiere, clic droit pour finir (3 points au moins)\n";
     std::vector<pixel> v;
-    int x,y,m=0;
+    int x,y,m;
     getMouse(x,y);
+    for (int i=0; i<2;i++){
+        v.push_back(pixel(x,y));
+        m=getMouse(x, y);
+        drawLine(x,y,v.back().getX(),v.back().getY(),RED);
+    }
     while (m==1) {
         v.push_back(pixel(x,y));
         m=getMouse(x, y);
@@ -23,9 +41,20 @@ void f(){
             drawLine(x,y,v.back().getX(),v.back().getY(),RED);
     }
     drawLine(v.front().getX(),v.front().getY(),v.back().getX(),v.back().getY(),RED);
-    endGraphics();
 
+    //
+
+
+
+
+
+
+
+
+    endGraphics();
+    return 0 ;
 }
 int main() {
+    f();    //test (a supprimer à votre convenance)
 	return 0;
 }
